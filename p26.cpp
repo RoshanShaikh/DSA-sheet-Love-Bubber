447d1160f496cce9a5ad0a9a506c2ae38e9bcfb1
RoshanShaikh <12roshanshaikh@gmail.com>
2022-05-24 15:26:50 +0530

    23-5-22

---
+++ p26.cpp
@@ -0,0 +1,39 @@
+// { Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+int maxProfit(vector<int> &price)
+{
+    // Write your code here..
+    int n = price.size();
+    vector<int> profit(n, 0);
+    int max_price = price[n - 1];
+    for (int i = n - 2; i >= 0; i--)
+    {
+        if (price[i] > max_price)
+            max_price = price[i];
+        profit[i] = max(profit[i + 1], max_price - price[i]);
+    }
+    int min_price = price[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (price[i] < min_price)
+            min_price = price[i];
+        profit[i] = max(profit[i - 1], profit[i] + (price[i] - min_price));
+    }
+
+    return profit[n - 1];
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+    vector<int> v = {10, 22, 5, 75, 65, 80};
+    cout << maxProfit(v);
+}
+// } Driver Code Ends
