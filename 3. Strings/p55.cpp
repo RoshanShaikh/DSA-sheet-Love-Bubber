b4c84e10d6c43ac14db09b61403f65a3a059c409
RoshanShaikh <12roshanshaikh@gmail.com>
2022-06-03 23:00:33 +0530

    3-6-22

---
+++ 3. Strings/p55.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    int longestCommonSubsequence(string text1, string text2)
+    {
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (text1[i - 1] == text2[j - 1] && i != j)
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+public:
+    int LongestRepeatingSubsequence(string str)
+    {
+        // Code here
+        return longestCommonSubsequence(str, str);
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        string str;
+        cin >> str;
+        Solution obj;
+        int ans = obj.LongestRepeatingSubsequence(str);
+        cout << ans << "\n";
+    }
+    return 0;
+} // } Driver Code Ends
